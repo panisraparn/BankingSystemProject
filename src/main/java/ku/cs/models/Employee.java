29f44198c63eb3bdfa68ca881ddd98da3ad43eb2
Parn panisra <Panisra.w@ku.th>
2022-10-29 19:03:13 +0700

    update emp model

--- src/main/java/ku/cs/models/Employee.java
+++ src/main/java/ku/cs/models/Employee.java
@@ -1,4 +1,19 @@
 package ku.cs.models;
 
 public class Employee {
+
+    private String emp_id;
+    private String emp_name;
+    private String emp_jTitle;
+    private String emp_password;
+
+    //add employee
+    public Employee(String emp_id, String emp_name, String emp_jTitle, String emp_password) {
+        this.emp_id = emp_id;
+        this.emp_name = emp_name;
+        this.emp_jTitle = emp_jTitle;
+        this.emp_password = emp_password;
+    }
+
+
 }
