6b9ec94cc48953adae139f5bc146785dd9718734
Parn panisra <Panisra.w@ku.th>
2022-10-29 21:46:09 +0700

    after

--- src/main/java/ku/cs/controllers/LoginController.java
+++ src/main/java/ku/cs/controllers/LoginController.java
@@ -1,9 +1,15 @@
 package ku.cs.controllers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import ku.cs.models.Employee;
+import ku.cs.services.DatabaseConnection;
 
-        import javafx.event.ActionEvent;
-        import javafx.fxml.FXML;
-        import javafx.scene.control.PasswordField;
-        import javafx.scene.control.TextField;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LoginController {
 
@@ -13,11 +19,36 @@ public class LoginController {
     @FXML
     private PasswordField passwordField;
 
+
+    //เป็น account ที่ไว้ใช้ login
+    public Employee empLoginAccount;
+
+
+
     @FXML
     void handleLoginButton(ActionEvent event) {
 
-    }
+        String emp_IdLoginStr = usernameTextField.getText();
+        String emp_passwordStr = passwordField.getText();
 
-}
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
+
+        String connectQuery = "SELECT Emp_id, Emp_name, Emp_password FROM employee WHERE Emp_id = '"+emp_IdLoginStr+"'";
+//        empLoginAccount = new (Emp_id);
+
+        try {
+            Statement statement = connectDB.createStatement();
+            ResultSet queryOutput = statement.executeQuery(connectQuery);
+
+            empLoginAccount = new(queryOutput.getString("Emp_id"), );
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
+    }
 
 
--- src/main/java/ku/cs/models/Employee.java
+++ src/main/java/ku/cs/models/Employee.java
@@ -16,4 +16,7 @@ public class Employee {
     }
 
 
+    public String toCsv() {
+        return emp_id + "," + emp_name + "," + emp_jTitle + ","  + emp_password;
+    }
 }
