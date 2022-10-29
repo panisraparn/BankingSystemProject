6e74a263eb060e7dd3352355b9972d32788e5d9f
Gust <sittikorn.au@ku.th>
2022-10-29 18:45:39 +0700

    add fxml home

--- src/main/java/ku/cs/App.java
+++ src/main/java/ku/cs/App.java
@@ -19,7 +19,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXRouter.bind(this, stage, "Loan System",800, 600);
         configRoute();
-        FXRouter.goTo("registration");
+        FXRouter.goTo("login");
     }
 
     private void configRoute() {
